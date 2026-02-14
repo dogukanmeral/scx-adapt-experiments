8eb2ada6403dc6d36accba6da85ac6420eca2616
Onur Karagür <116795501+Onurk77@users.noreply.github.com>
2026-02-14 23:28:33 +0300

    Update concat_dataset.py

--- ml/src/concat_dataset.py
+++ ml/src/concat_dataset.py
@@ -1,9 +1,37 @@
 # Import libraries
+from pathlib import Path
+import pandas as pd
 
-# Create datasets folder in ml/
+# Define strings
+PROJECT_ROOT = Path(__file__).resolve().parent.parent.parent
+UPPER_DATASETS = PROJECT_ROOT / "datasets"
+ML_DATASETS = PROJECT_ROOT / "ml" / "datasets"
+
+OUTPUT_FILE = ML_DATASETS / "combined_dataset.csv"
+
+# Create datasets folder in ml/datasets if missing
+def create_dataset_folders():
+
+    ML_DATASETS.mkdir(parents=True, exist_ok=True)
+    print("Created datasets folder.")
 
 # Read every csv from datasets and create dataFrames
+csv_files = list(UPPER_DATASETS.glob("*.csv"))
 
+if not csv_files:
+    raise ValueError(f"No csv files in {UPPER_DATASETS}")
+
+dfs = []
+
+for file in csv_files:
+    print(f"Reading: {file.name}")
+    df = pd.read_csv(file)
+
+    # Keep source file name info
+    df["source_file"] = file.name
+
+    dfs.append(df)
+    
 # Add id, scheduler, task type (cpu-io-mem)
 
 # Concatenate results
