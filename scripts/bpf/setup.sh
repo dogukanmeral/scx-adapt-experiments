30f442102cbd1df866cd319eab29d3d77bc731aa
Doğukan Meral <dogukan.meral@yahoo.com>
2026-03-04 00:15:26 +0300

    Update build.sh and setup.sh

--- scripts/bpf/setup.sh
+++ scripts/bpf/setup.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # Generate the vmlinux.h file to start using in the IDE
 
-bpftool btf dump file /sys/kernel/btf/vmlinux format c > sample-bpf/include/scx/vmlinux.h
+bpftool btf dump file /sys/kernel/btf/vmlinux format c | tee sample-bpf/include/scx/vmlinux.h > sample-bpf/minimal-scheduler/vmlinux.h
